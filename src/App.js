01782ca9fe587ff550cc6c3356029e1ccaef5a1d
Noelle Laureano <nrlaureano@gmail.com>
2020-03-22 21:49:03 -0400

    starting point

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-// (1) import in react router
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+// (2) import Link and Route from react router
+import { Link, Route } from 'react-router-dom'
 import './App.css'
 
 import { Bio, Experience, Projects, Contact } from './Components'
@@ -15,7 +15,7 @@ function App() {
             <li><Link to='/'>Noelle Laureano</Link></li>
           </div>
           <div className='nav-menu'>
-            {/* (3) update links */}
+            {/* (4) update links */}
             <li><Link to='/projects'>Projects</Link></li>
             <li><Link to='/experience'>Experience</Link></li>
             <li><Link to='/contact'>Contact</Link></li>
@@ -23,13 +23,11 @@ function App() {
         </ul>
       </nav>
 
-      {/* (2) Routes */}
-      <Router>
-        <Route exact path='/' component={Bio} />
-        <Route path='/projects' component={Projects} />
-        <Route path='/experience' component={Experience} />
-        <Route path='/contact' component={Contact} />
-      </Router>
+      {/* (3) Routes */}
+      <Route exact path='/' component={Bio} />
+      <Route path='/projects' component={Projects} />
+      <Route path='/experience' component={Experience} />
+      <Route path='/contact' component={Contact} />
 
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// (1) import BrowserRouter from react router
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import './index.css';
