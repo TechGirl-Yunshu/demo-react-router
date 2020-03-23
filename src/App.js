9958fba88bfa648bb95be541f3ff99bcdbb7a191
Noelle Laureano <nrlaureano@gmail.com>
2020-03-22 22:00:22 -0400

    starting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// (2) import Link and Route from react router
+// import Link and Route from react router
 import { Link, Route } from 'react-router-dom'
 import './App.css'
 
@@ -15,7 +15,7 @@ function App() {
             <li><Link to='/'>Noelle Laureano</Link></li>
           </div>
           <div className='nav-menu'>
-            {/* (4) update links */}
+            {/* update links */}
             <li><Link to='/projects'>Projects</Link></li>
             <li><Link to='/experience'>Experience</Link></li>
             <li><Link to='/contact'>Contact</Link></li>
@@ -23,7 +23,8 @@ function App() {
         </ul>
       </nav>
 
-      {/* (3) Routes */}
+      {/* Routes */}
+      {/* (1) inspect implicit route props */}
       <Route exact path='/' component={Bio} />
       <Route path='/projects' component={Projects} />
       <Route path='/experience' component={Experience} />
